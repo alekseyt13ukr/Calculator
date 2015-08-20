bee05b73a8fb7985b62b5301e43c7e6cab8c6529
alekseyt13ukr <alekseyt.ukr@mail.ru>
2015-08-20 20:30:21 +0300

    Added string parser functionality

---
+++ src/com/taran/leason1/model/Minus.java
@@ -0,0 +1,8 @@
+package com.taran.leason1.model;
+
+public class Minus {
+
+    public double minus(double summand1, double summand2) {
+        return summand1 - summand2;
+    }
+}
---
+++ src/com/taran/leason1/model/Reader.java
@@ -0,0 +1,13 @@
+package com.taran.leason1.model;
+
+import java.util.Scanner;
+
+public class Reader {
+    public String readMathString() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input math string");
+        String mathString = scanner.nextLine();
+
+        return mathString;
+    }
+}
